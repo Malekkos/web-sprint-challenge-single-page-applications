9dedf3ef59f0065c7244a7a707938ce648ec0bf6
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-10 00:12:14 -0700

    Added a link wrapper for minor styling purposes

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ const App = () => {
       <div className="topWrapper">
         <nav className="nav">
           <h1 id="Headermain" className="Headermain">Carpachos Carparchiest Pizza!</h1>
-          <Link id="order-pizza" className="homeLink" to="/" >Home</Link>
-          <Link id="pizza-form" className="pizzaLink" to="pizza" >Make a Pizza!</Link>
+          <div className="linkWrapper">
+            <Link id="order-pizza" className="homeLink" to="/" >Home</Link>
+            <Link id="pizza-form" className="pizzaLink" to="pizza" >Make a Pizza!</Link>
+          </div>
           {/* <Link to="pizzaCreator">Make a Pie!</Link> */}
         </nav>
       </div>
