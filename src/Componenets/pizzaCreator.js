2545228172f60b06ac63eb9044b379fea869ff8a
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-10 00:30:47 -0700

    Setup a state and change handler, but they need to be changed later to be DRY. Setup basic markup for the page as well as most of the options I will be giving for creation

--- src/Componenets/pizzaCreator.js
+++ src/Componenets/pizzaCreator.js
@@ -1,10 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 
 const PizzaCreator = (props) => {
-
+  
+  const [size,setSize] = useState("")
+  const [name, setName] = useState("")
+  const handleChangeSize = (event) => {
+    setSize(event.target.value);
+  }
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  }
   return (
     <>
-      <div>Hello!</div>
+      <div>
+        <h2>Hello! Lets make your pizza!</h2>
+      </div>
+      <div>
+        <label>Your Name
+          <input onChange={handleChangeName} type="text" id="name-input" />
+        </label>
+        <label>What size pizza?
+          <select onChange={handleChangeSize} type="dropdown" id="size-dropdown" >
+            <option value="">Choose a size..</option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </label>
+        <label>What Toppings?
+          <input type="checkbox" value="pepperoni" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+        </label>
+      </div>
     </>
   )
 
