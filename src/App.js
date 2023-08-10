d3e5f2558f94b90e6cdccf904a76db0138cf51de
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-09 23:00:59 -0700

    Added several components, added several imports, added a basic nav inside of App and wrapped my App in ReactRouter from index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { Routes, Route, Link } from "react-router-dom"
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div className="topWrapper">
+        <nav>
+          <h1 id="Headermain" className="Headermain">Carpachos Carparchiest Pizza!</h1>
+          <Link to="/" >Home</Link>
+          {/* <Link to="pizzaCreator">Make a Pie!</Link> */}
+        </nav>
+      </div>
+      <Routes>
+        <Route to="/" />
+      </Routes>
     </>
   );
 };
