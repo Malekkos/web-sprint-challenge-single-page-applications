9ccb8d40bfa2dbcbca1e0c00e16ac65ec7199f38
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-09 23:34:57 -0700

    Added some classnames and styling, changed browserRouter to be in app due to some errors(will revisit), Added some content to Home

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./Componenets/Home"
+
+
+
+
 const App = () => {
   return (
     <>
+    <BrowserRouter>
       <div className="topWrapper">
-        <nav>
+        <nav className="nav">
           <h1 id="Headermain" className="Headermain">Carpachos Carparchiest Pizza!</h1>
-          <Link to="/" >Home</Link>
+          <Link className="homeLink" to="/" >Home</Link>
           {/* <Link to="pizzaCreator">Make a Pie!</Link> */}
         </nav>
       </div>
       <Routes>
-        <Route to="/" />
+        <Route path="/" element={<Home />} />
       </Routes>
+    </BrowserRouter>
     </>
   );
 };
--- src/Componenets/Home.js
+++ src/Componenets/Home.js
@@ -1,13 +1,22 @@
 import React from "react"
 
 
-const Home = () => {
+const Home = (props) => {
 
   return (
     <>
-    
+    <div className="backgroundcolor-home" >
+      <div className="HomeWrapper">
+        <h3>Get a taste of what we are serving..</h3>
+        <img className="home-picture" src="https://media.istockphoto.com/id/943449226/photo/concept-promotional-flyer-and-poster-for-restaurants-or-pizzerias-template-with-delicious.jpg?s=2048x2048&w=is&k=20&c=trEQAX4_SPQmtVl_alnnqq2XKLw3RcR8YggmKEyRGuk=" alt="Floating Pizza"   />
+      </div>
+    </div>
     </>
   )
 
 
 }
+
+
+
+export default Home
